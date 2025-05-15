7bd6435f56268f62daa44b5641535b3ed5f04f02
kangsulim <rkdtnfla1@naver.com>
2025-05-15 17:42:22 +0900

    RESTful API

---
+++ cardatabase3/src/main/java/com/packt/cardatabase/domain/Car.java
@@ -0,0 +1,99 @@
+package com.packt.cardatabase.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String brand, model, color, registrationNumber;
+
+    private int modelYear, price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="owner")       // 조인할 때 쓸 컬럼인데 컬럼명을 owner로 지은 것
+    private Owner owner;
+    // @JoinColumn만 사용할 경우 하이버네이트에서 pk_fk 형태로 임의의 컬럼명을 생성항 -> ownerId_owner 등
+
+    // 기본 생성자.
+    public Car() {
+    }
+
+    // id 필드를 제외한 나머지 필드에 대한 RequiredArgsConstructor.
+    public Car(String brand, String model, String color, String registrationNumber, int modelYear, int price, Owner owner) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registrationNumber = registrationNumber;
+        this.modelYear = modelYear;
+        this.price = price;
+        this.owner = owner;
+    }
+
+    // Getter / Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public int getModelYear() {
+        return modelYear;
+    }
+
+    public void setModelYear(int modelYear) {
+        this.modelYear = modelYear;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+}
